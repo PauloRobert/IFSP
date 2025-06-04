93e8738920e40a2bd0b633768d45c2390f0de352
Paulo Roberto <evans_sp@hotmail.com>
2025-06-04 09:12:39 -0300

    Add variáveis

--- Pós Algoritmos e Programação - WEB/Módulo_2/main.c
+++ Pós Algoritmos e Programação - WEB/Módulo_2/main.c
@@ -2,6 +2,12 @@
 
 int main() {
     char nomePaciente[100];
-    printf("Nome do paciente: ");
-    fgets(nomePaciente, sizeof(nomePaciente), stdin);
+    char medicamento1[50], medicamento2[50], medicamento3[50];
+    float dose1, vezes1, dose2, vezes2, dose3, vezes3;
+    float total1, total2, total3, totalGeral;
+
+    printf("=== CONTROLE DE MEDICAMENTOS ===\n");
+        printf("Nome do paciente: ");
+        fgets(nomePaciente, sizeof(nomePaciente), stdin); // captura nome com espaços
+
 }
