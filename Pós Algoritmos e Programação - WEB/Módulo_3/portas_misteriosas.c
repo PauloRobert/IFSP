2e58f70804f8ec6b47902af83f4b51bf03f421e2
Paulo Roberto <evans_sp@hotmail.com>
2025-06-15 01:34:04 -0300

    feat: adiciona lógica de sorteio e verificação da porta premiada

--- Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
+++ Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
@@ -3,25 +3,35 @@
 #include <time.h>
 
 int main() {
-
     int vidas = 3;
     int pontos = 0;
-    int escolha;
+    int escolha, portaPremiada;
+
+    // Inicializa o gerador de números aleatórios
+    srand(time(NULL));
 
     printf("=== Jogo das Portas Misteriosas ===\n");
     printf("Você tem 3 vidas. Escolha a porta certa para ganhar pontos!\n");
 
     while (vidas > 0) {
+        portaPremiada = (rand() % 3) + 1; // 1 a 3
 
-            printf("Escolha uma porta (1, 2 ou 3): ");
-            scanf("%d", &escolha);
+        printf("Escolha uma porta (1, 2 ou 3): ");
+        scanf("%d", &escolha);
 
-            printf("Você escolheu a porta %d\n", escolha);
-            vidas--; // teste para ver o loop funcionar
-
+        if (escolha == portaPremiada) {
+            printf("Parabéns! Você acertou a porta premiada!\n");
+            pontos++;
+        } else {
+            printf("Que pena! A porta premiada era a %d.\n", portaPremiada);
+            vidas--;
         }
 
-        printf("Fim de jogo! Sua pontuação final foi: %d\n", pontos);
+        printf("Vidas: %d | Pontos: %d\n", vidas, pontos);
+        printf("-------------------------------\n");
+    }
+
+    printf("Fim de jogo! Sua pontuação final foi: %d\n", pontos);
 
     return 0;
 }
