bb62131898217de8fe4af2fe3b0d1416e43d9781
Paulo Roberto <evans_sp@hotmail.com>
2025-06-15 01:39:24 -0300

    refactor: adiciona comentários e melhora mensagens de interface

--- Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
+++ Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
@@ -5,30 +5,40 @@
 int main() {
     int vidas = 3;
     int pontos = 0;
-    int escolha, portaPremiada;
+    int portaPremiada, escolha;
 
     // Inicializa o gerador de números aleatórios
     srand(time(NULL));
 
     printf("=== Jogo das Portas Misteriosas ===\n");
-    printf("Você tem 3 vidas. Escolha a porta certa para ganhar pontos!\n");
+    printf("Você tem 3 vidas. Escolha a porta certa para ganhar pontos!\n\n");
 
     while (vidas > 0) {
-        portaPremiada = (rand() % 3) + 1; // 1 a 3
+        // Sorteia a porta premiada (1, 2 ou 3)
+        portaPremiada = (rand() % 3) + 1;
 
+        // Mostra as opções ao jogador
         printf("Escolha uma porta (1, 2 ou 3): ");
         scanf("%d", &escolha);
 
-        if (escolha == portaPremiada) {
-            printf("Parabéns! Você acertou a porta premiada!\n");
-            pontos++;
-        } else {
-            printf("Que pena! A porta premiada era a %d.\n", portaPremiada);
-            vidas--;
+        // Verifica se a entrada é válida
+        if (escolha < 1 || escolha > 3) {
+            printf("Escolha inválida! Digite 1, 2 ou 3.\n");
+            continue; // volta para o começo do while
         }
 
-        printf("Vidas: %d | Pontos: %d\n", vidas, pontos);
-        printf("-------------------------------\n");
+        // Verifica se o jogador acertou
+        if (escolha == portaPremiada) {
+            pontos++;
+            printf("Parabéns! Você escolheu a porta certa!\n");
+        } else {
+            vidas--;
+            printf("Ops! A porta premiada era a %d. Você perdeu uma vida.\n", portaPremiada);
+        }
+
+        // Exibe o status atual
+        printf("Vidas restantes: %d | Pontos: %d\n", vidas, pontos);
+        printf("----------------------------------\n");
     }
 
     printf("Fim de jogo! Sua pontuação final foi: %d\n", pontos);
