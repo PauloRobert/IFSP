33d5858275597d2bdf6131a040ce9b903b828e66
Paulo Roberto <evans_sp@hotmail.com>
2025-06-15 01:04:53 -0300

    Criando a estrutura do jogo

---
+++ Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("=== Jogo das Portas Misteriosas ===\n");
+    printf("Você tem 3 vidas. Escolha a porta certa para ganhar pontos!\n");
+
+    return 0;
+}
