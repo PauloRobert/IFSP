3453b8ff28f8960b0f261291a69bbe60550d26b5
Paulo Roberto <evans_sp@hotmail.com>
2025-06-15 01:27:31 -0300

    feat: adiciona contadores de vida e pontuação com laço while básico

--- Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
+++ Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
@@ -4,5 +4,12 @@ int main() {
     printf("=== Jogo das Portas Misteriosas ===\n");
     printf("Você tem 3 vidas. Escolha a porta certa para ganhar pontos!\n");
 
+    while (vidas > 0) {
+            // Aqui será a lógica do jogo
+            printf("Ainda temos %d vidas...\n", vidas);
+        }
+
+        printf("Fim de jogo! Sua pontuação final foi: %d\n", pontos);
+
     return 0;
 }
