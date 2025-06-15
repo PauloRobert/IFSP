c3ca680911fbbec647ea56b18b739bac4bd9df76
Paulo Roberto <evans_sp@hotmail.com>
2025-06-15 01:29:33 -0300

    feat: adiciona leitura da escolha do jogador com scanf

--- Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
+++ Pós Algoritmos e Programação - WEB/Módulo_3/portas_misteriosas.c
@@ -10,8 +10,8 @@ int main() {
     printf("Você tem 3 vidas. Escolha a porta certa para ganhar pontos!\n");
 
     while (vidas > 0) {
-            // Aqui será a lógica do jogo
-            printf("Ainda temos %d vidas...\n", vidas);
+            printf("Escolha uma porta (1, 2 ou 3): ");
+
         }
 
         printf("Fim de jogo! Sua pontuação final foi: %d\n", pontos);
